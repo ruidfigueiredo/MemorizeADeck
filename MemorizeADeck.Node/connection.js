04d6e50a1f58010ddb313f762b7a663af65b4d03
Rui <rui.d.figueiredo@gmail.com>
2020-03-24 22:10:39 +0000

    Removed memorizeadeck.node and increased height for options
    The options height didn't look right in windows (unicode characters are different)

--- MemorizeADeck.Node/connection.js
+++
@@ -1,4 +0,0 @@
-const { ConnectionBuilder } = require('electron-cgi');
-
-exports.connection = new ConnectionBuilder().connectTo('dotnet', 'run', '--project', '../MemorizeADeck.DotNet/MemorizeADeck.ElectronCgiConnect').build();
-
